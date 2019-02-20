90d3042c66627f08a84e7758283ebd7e0c9aa011
noam <nodan816@gmail.com>
2019-02-20 20:07:03 +0200

    gjhgthjgh

--- Shope/Startup.cs
+++ Shope/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Shope.Models;
 
 namespace Shope
 {
@@ -22,6 +24,9 @@ namespace Shope
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<ShopeContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ShopeContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +48,7 @@ namespace Shope
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Customers}/{action=Index}/{id?}");
             });
         }
     }
